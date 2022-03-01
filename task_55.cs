3c2d08201775e109a18a473a8fcd15b6896023f9
kurrganskaya <66162354+kurrganskaya@users.noreply.github.com>
2022-03-01 14:20:29 +0300

    task 55

---
+++ task_55.cs
@@ -0,0 +1,44 @@
+// Задача 55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void FillArray(int[,] massiv)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+FillArray(array);
+
+void PrintArray(int[,] massiv)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{massiv[i, j]}  " + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
+
+Console.WriteLine();
+for (int j = 0; j < n; j++)
+{
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + array[i, j];
+    }
+    Console.WriteLine($"Ср. арифм. {j + 1} столбца: {(double)sum / n}.\n");
+    sum = 0;
+}
